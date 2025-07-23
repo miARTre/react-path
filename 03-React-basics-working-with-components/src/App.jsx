579136e7c5497d9b8c5e1d758ee1cface7b4906f
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-23 14:56:15 +0200

    Set up React entry point and basic App component

---
+++ 03-React-basics-working-with-components/src/App.jsx
@@ -0,0 +1,52 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
+
+function App() {
+  let tabContent = "Please click a button!";
+
+  function handleSelect(selectedButton) {
+    // selectedButton => "Components", 'jsx', ....
+    // console.log(selectedButton);
+    tabContent = selectedButton;
+    console.log(tabContent)
+  }
+
+  console.log("APP COMPONENT EXECUTING'")
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {tabContent}
+        </section>
+        <h2>Time to get started!</h2>
+      </main>
+    </div>
+  );
+}
+
+export default App;
