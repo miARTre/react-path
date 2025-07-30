11b3fd1fc219c3831c3e57dd97a31b608b37f143
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-07-30 12:44:07 +0200

    Added GameOver and Log components, updated GameBoard and Player logic

--- 05-Rendering-Lists-Conditional-Content/src/components/GameBoard.jsx
+++ 05-Rendering-Lists-Conditional-Content/src/components/GameBoard.jsx
@@ -1,33 +1,29 @@
-import { useState } from "react";
+export default function GameBorad({ onSelectSquare, board }) {
+  // const [gameBorad, setGameBoard] = useState(initialGameBorad);
 
-const initialGameBorad = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
+  // function handleSelectSquare(rowIndex, colIndex) {
+  //   setGameBoard((prevGameBoard) => {
+  //     const updatedBoard = [
+  //       ...prevGameBoard.map((innerArray) => [...innerArray]),
+  //     ];
+  //     updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+  //     return updatedBoard;
+  //   });
 
-export default function GameBorad({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBorad, setGameBoard] = useState(initialGameBorad);
+  //   onSelectSquare();
+  // }
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
-
-    onSelectSquare();
-  }
   return (
     <ol id="game-board">
-      {gameBorad.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
                   {playerSymbol}
                 </button>
               </li>
