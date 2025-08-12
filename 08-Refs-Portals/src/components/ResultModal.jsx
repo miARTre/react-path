3494afb04e71c63f78b2518fc4827f661e2a2cdd
Mirnes Becirbegovic <mirnesbb@gmail.com>
2025-08-12 10:29:40 +0200

    refactor(ResultModal): expose open() via useImperativeHandle
    Convert ResultModal to a forwardRef component and use useImperativeHandle
    to provide an open() method for parent control.

--- 08-Refs-Portals/src/components/ResultModal.jsx
+++ 08-Refs-Portals/src/components/ResultModal.jsx
@@ -1,6 +1,38 @@
-export default function ResultModal({ result, targetTime }) {
+// export default function ResultModal({ ref, result, targetTime }) {
+//   return (
+//     <dialog ref={ref} className="result-modal">
+//       <h2>You {result}</h2>
+//       <p>
+//         The target time was <strong>{targetTime} seconds.</strong>
+//       </p>
+//       <p>
+//         You stopped the timer with <strong>X seconds left.</strong>
+//       </p>
+//       <form method="dialog">
+//         <button>Close</button>
+//       </form>
+//     </dialog>
+//   );
+// }
+
+import { forwardRef, useImperativeHandle, useRef } from "react";
+
+const ResultModal = forwardRef(function ResultModal(
+  { result, targetTime },
+  ref
+) {
+  const dialog = useRef();
+
+  useImperativeHandle(ref, () => {
+    return {
+      open() {
+        dialog.current.showModal();
+      },
+    };
+  });
+
   return (
-    <dialog className="result-modal" open>
+    <dialog ref={dialog} className="result-modal">
       <h2>You {result}</h2>
       <p>
         The target time was <strong>{targetTime} seconds.</strong>
@@ -13,4 +45,6 @@ export default function ResultModal({ result, targetTime }) {
       </form>
     </dialog>
   );
-}
+});
+
+export default ResultModal;
